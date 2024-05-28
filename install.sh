506a61d38ae7affc85bb2f01ed368215780a07f5
rjm <robjmass@gmail.com>
2024-05-28 18:52:00 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,33 @@
 
 dir="$HOME/dotfiles"
 
-for file in "$dotfiles_dir"/*; do
+ignores=("install.sh" ".gitignore" ".gitmodules")
+
+for file in "$dir"/*; do
     filename=$(basename "$file")
 
     if [ -d "$file" ]; then
         continue
     fi
-    ln -s "$file" "$HOME/filename"
+
+    ignorable=false
+    for ignore in "${ignores[@]}"; do
+        if [ "$filename" = "$ignore" ]; then
+            ignorable=true
+            break
+        fi
+    done
+    if [ "$ignorable" = true ]; then
+        echo "ignoring $filename"
+        continue
+    fi
+
+    if [ -e "$HOME/$filename" ]; then
+        echo "$filename already exists"
+        continue
+    fi
+
+    #ln -s "$file" "$HOME/filename"
 done
 
 echo "Done"
