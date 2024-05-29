484900f198c176b2d1e419a4419097a2d0cf09f3
rjm <robjmass@gmail.com>
2024-05-29 13:30:46 -0400

    zsh plugin install fix

--- install.sh
+++ install.sh
@@ -51,8 +51,9 @@ for file in "$dir"/*; do
     ln -s "$file" "$HOME/$filename"
     echo "Created symbolic link to $filename"
 done
-zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+ln -sf $dir/.zshrc $HOME/.zshrc
 
 
 
