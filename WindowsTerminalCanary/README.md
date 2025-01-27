3e482241a43260caa65448f25d7c69ad16ad51b3
rmassaroni <robjmass@gmail.com>
2025-01-27 14:43:49 -0500

    installation readme

---
+++ WindowsTerminalCanary/README.md
@@ -0,0 +1,7 @@
+## Windows Terminal Canary Setup
+
+
+
+```Admin Command Prompt
+curl -o install.bat https://raw.githubusercontent.com/bulkOS/arm/main/WindowsTerminalCanary/install.bat
+```
--- WindowsTerminalCanary/install.sh
+++
@@ -1,4 +0,0 @@
-# cross platform method. prob doesnt work
-cmd.exe /C "del C:\\Users\\rjm\\AppData\\Local\\Packages\\Microsoft.WindowsTerminalCanary_8wekyb3d8bbwe\\LocalState\\settings.json && mklink C:\\Users\\rjm\\AppData\\Local\\Packages\\Microsoft.WindowsTerminalCanary_8wekyb3d8bbwe\\LocalState\\settings.json \\\\wsl$\\Ubuntu\\home\\rjm\\dotfiles\\WindowsTerminal\\settings.json"
-
-echo "Windows Terminal symlink setup completed."
