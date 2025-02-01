de609db87229dbe8629bce5a674d17dcc5a30eee
rmassaroni <robjmass@gmail.com>
2025-02-01 18:32:31 -0500

    install dir

---
+++ install/symlink.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# in old script, this was done before starting zsh
+
+
+ignores=("install.sh" ".gitignore" ".gitmodules" "README.md" "zap.zsh")
+shopt -s dotglob
+
+for file in "$dir"/*; do
+    filename=$(basename "$file")
+
+    if [ -d "$file" ]; then
+        continue
+    fi
+
+    ignorable=false
+    for ignore in "${ignores[@]}"; do
+        if [ "$filename" = "$ignore" ]; then
+            ignorable=true
+        fi
+    done
+    if [ "$ignorable" == true ]; then
+        echo "ignoring $filename"
+        continue
+    fi
+
+    if [ -e "$HOME/$filename" ]; then
+        # read -p "$filename already exists. Do you want to override it? (y/n): " answer
+        # case $answer in
+        #     [Yy]* )
+        #         ln -sf "$file" "$HOME/$filename"
+        #         echo "Overridden $filename"
+        #         ;;
+        #     [Nn]* )
+        #         echo "Skipped $filename"
+        #         ;;
+        #     * )
+        #         echo "Invalid input. Skipping $filename"
+        #         ;;
+        # esac
+        echo "Overriding existing $filename..."
+        ln -sf "$file" "$HOME/$filename"
+        echo "Created symbolic link to $filename"
+        continue
+    fi
+
+    ln -s "$file" "$HOME/$filename"
+    echo "Created symbolic link to $filename"
+done
+
