496f4a80ec6b3fe76d6151fdbfe91420b3d4791b
rmassaroni <robjmass@gmail.com>
2025-02-01 20:04:51 -0500

    install fix

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ if [ -f "$HOME/.zshrc" ]; then
 fi
 
 echo "Linking .zshrc from dotfiles..."
-ln -s "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+ln -s "$dir/.zshrc" "$HOME/.zshrc"
 
 
 # Check if Zsh is installed
