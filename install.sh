8217bf9ddc2342ce9927f81c7d24019c060e4071
rmassaroni <robjmass@gmail.com>
2025-02-01 20:47:13 -0500

    install fix

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@ shopt -s dotglob #include hidden filenames in searches.
 # add these to zshrc or bashrc to make it universal
 
 
-
-
-
 # GPUSH
 bash <(curl https://raw.githubusercontent.com/rmassaroni/gpush/main/install.sh)
 
@@ -45,6 +42,8 @@ then
 else
     echo "Zsh is already installed."
 fi
+
+
 # Set Zsh as the default shell if it isn't already
 if [ "$SHELL" != "$(which zsh)" ]; then
     echo "Setting Zsh as the default shell..."
@@ -52,6 +51,17 @@ if [ "$SHELL" != "$(which zsh)" ]; then
 else
     echo "Zsh is already the default shell."
 fi
+
+
+# Check if Zap is already installed
+if ! grep -q "zap.zsh" "$HOME/.zshrc"; then
+    echo "Zap not found in .zshrc. Installing Zap..."
+    # Install Zap using the provided installation script
+    zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+else
+    echo "Zap is already installed."
+fi
+
 
 echo "Installation complete."
 
