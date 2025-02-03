dfa11062b686ba9fa075415a01ada099e830e893
rmassaroni <robjmass@gmail.com>
2025-02-03 13:05:57 -0500

    install fix

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 
+
+
+
+
+echo "Sourcing zap.sh"
+source "$install_dir/zap.sh" # this will probably be sourced from zsh.sh
+
+
+
+
 # Check if Zsh is installed
 if ! command -v zsh &> /dev/null
 then
