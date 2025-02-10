e1b732311d0b47fe5a04cba65fc2dcaadd540043
rmassaroni <robjmass@gmail.com>
2025-02-10 11:36:28 -0500

    check for existing files

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -3,6 +3,8 @@
 shopt -s dotglob # include hidden files
 
 dir="$HOME/dotfiles"
+backup_dir="$HOME/dotfiles/local-backups"
+mkdir -p "$backup_dir" # create backup directory if it doesn't exist
 
 linkables=(".aliases" ".bash_logout" ".bashrc" ".gitconfig" ".p10k.zsh" ".scripts.sh" ".vimrc")
 # linkable directory?
@@ -15,15 +17,41 @@ for file in "$dir"/*; do
         continue
     fi
 
+    # for linkable in "${linkables[@]}"; do
+    #     if [ "$filename" = "$linkable" ]; then
+    #         echo "Creating symbolic link to $filename"
+    #         ln -s "$file" "$HOME/$filename"
+    #     fi
+    # done
     for linkable in "${linkables[@]}"; do
         if [ "$filename" = "$linkable" ]; then
+            # Check if the file already exists in the home directory
+            if [ -e "$HOME/$filename" ]; then
+                # Compare the existing file with the new one
+                if cmp -s "$file" "$HOME/$filename"; then
+                    # Files are identical, delete the existing file
+                    echo "Deleting identical file: $HOME/$filename"
+                    rm "$HOME/$filename"
+                else
+                    # Files are different, move the existing file to local-backups
+                    if [ -e "$backup_dir/$filename" ]; then
+                        echo "Deleting existing backup: $backup_dir/$filename"
+                        rm "$backup_dir/$filename"
+                    fi
+                    echo "Moving old file to backup: $HOME/$filename"
+                    mv "$HOME/$filename" "$backup_dir/"
+                fi
+            fi
+
+            # Create symbolic link to the new file
             echo "Creating symbolic link to $filename"
             ln -s "$file" "$HOME/$filename"
         fi
     done
+
 done
 
-
+# will this checking process work with existing symlinks?
 
 
 
